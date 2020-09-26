0fcec93f14bcab4bc6b2c0b3f38331b1a726bdd8
Dilip Parmar <dilip_parmar@mcafee.com>
2020-09-26 15:23:26 +0530

    Added iOS unit test target

---
+++ NetKit_iOSTests/NetKit_iOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  NetKit_iOSTests.swift
+//  NetKit_iOSTests
+//
+//  Created by dparmar on 9/26/20.
+//  Copyright © 2020 Dilip Parmar. All rights reserved.
+//
+
+import XCTest
+
+class NetKit_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
